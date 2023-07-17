8cb124000c2862cc6b02d69518f2bcef170d1f46
GyawaliSujan <sujangyawali2010@gmail.com>
2023-07-17 17:44:55 -0500

    signals.py created to make django signal to create user profile

---
+++ accounts/signals.py
@@ -0,0 +1,22 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, pre_save
+from .models import User, UserProfile
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender,instance,created, **kwargs):
+    print(created)
+    if created:
+        print('User created create user profile')
+        UserProfile.objects.create(user=instance)
+        print('user profile is created')
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            profile = UserProfile.objects.create(user=instance)
+            
+@receiver(pre_save, sender=User)
+def pre_save_profile_receiver(sender, instance, **kwargs):
+    print(instance.username, "this username is being saved")
+    
