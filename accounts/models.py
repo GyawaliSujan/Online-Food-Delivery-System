c6ab14dccaba3b56ebe45e8ca99467e8073cbaeb
GyawaliSujan <sujangyawali2010@gmail.com>
2023-07-16 20:30:30 -0500

    changes in account.models.py and media configuration

--- accounts/models.py
+++ accounts/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    def create_user(self, first_name, last_name, user_name, email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError("User must have an email address")
         
-        if not user_name:
+        if not username:
             raise ValueError("User must have username")
         
         user = self.model(
             email = self.normalize_email(email),
-            username = user_name,
+            username = username,
             first_name = first_name,
             last_name = last_name,
         )
@@ -20,10 +20,10 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_super_user(self, first_name, last_name, user_name, email, password=None):
+    def create_superuser(self, first_name, last_name, username, email, password=None):
         user = self.create_user(
-            email = self.normalize_email,
-            username = user_name,
+            email = self.normalize_email(email),
+            username = username,
             password = password,
             first_name = first_name,
             last_name = last_name
@@ -40,14 +40,14 @@ class User(AbstractBaseUser):
     CUSTOMER = 2
     ROLE_CHOICE = (
         (RESTAURANT, 'Restaurant'),
-        (CHOICE, 'choice')
+        (CUSTOMER, 'customer')
     )
     username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.IntegerField(max_length=20, blank=True)
-    role = models.PositiveSmallIntegerField(choice = ROLE_CHOICE, blank=True, null=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    role = models.PositiveSmallIntegerField(choices = ROLE_CHOICE, blank=True, null=True)
     
     #Django required fields
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -65,10 +65,28 @@ class User(AbstractBaseUser):
     objects = UserManager()
     
     def __str__(self):
-        return self.username
+        return self.email
     
     def has_perm(self, perm, obj=None):
         return self.is_admin
     
     def has_module_perms(self, app_label):
         return True
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pic', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photo', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=20, blank=True, null=True)
+    state = models.CharField(max_length=30, blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+    pin_code = models.IntegerField(blank=True, null=True)
+    latitude = models.CharField(max_length=30, blank=True, null=True)
+    longitude = models.CharField(max_length=30, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
